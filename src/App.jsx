c0ef959559ac6657cb44123cc69f5df5bd0c9432
TranLeLong <longprodao123@gmail.com>
2025-06-30 10:06:07 +0700

    dark_light_b1

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,42 @@
 import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
 import HomeIcon from '@mui/icons-material/Home'
 import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
+import Avatar from '@mui/material/Avatar'
 import Button from '@mui/material/Button'
-import { pink } from '@mui/material/colors'
+import { deepOrange, deepPurple, pink } from '@mui/material/colors'
+import { useColorScheme } from '@mui/material/styles'
+import * as React from 'react'
 import { useState } from 'react'
+
+// hàm để chuyển hình nền sang trắng , đen
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme(); // đã hỗ trợ lưu local storage
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light');
+        // localStorage.setItem('trello-dark-light-mode')
+        // localStorage.GetItem('trello-dark-light-mode')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  );
+}
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <div className="">Trần Lê Long</div>
+      <ModeToggle/>
+      <hr></hr>
+      <div className="">
+        <Avatar>H</Avatar>
+        <Avatar sx={{ bgcolor: deepOrange[500] }}>N</Avatar>
+        <Avatar sx={{ bgcolor: deepPurple[500] }}>OP</Avatar>
+      </div>
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
---
+++ src/theme.js
@@ -0,0 +1,23 @@
+// import { red } from '@mui/material/colors'
+// import { createTheme } from '@mui/material/styles'
+import { experimental_extendTheme as extendTheme } from '@mui/material/styles';
+// Create a theme instance.
+const theme = extendTheme({
+  useColorScheme : {
+    light: {
+      palette: {
+        primary: {
+          main: '#ff5252',
+        }
+      }
+    },
+    dark : {
+      palette: {
+        primary: {
+          main: '#000'
+        }
+      }
+    } 
+  }
+});
+export default theme;
